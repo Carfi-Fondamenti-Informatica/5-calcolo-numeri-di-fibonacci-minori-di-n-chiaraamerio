518e0626b04a8a71abd5baca85dd543fb0410ceb
chiaraamerio <100305899+chiaraamerio@users.noreply.github.com>
2022-04-03 19:10:30 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,16 @@
 using namespace std;
 
 int main() {
+int n=0,a=0, b=1, c;
+cout<<"inserire un numero intero n"<<endl;
+cin>>n;
+
+do {
+    c = a + b;
+    cout << c << endl;
+    b = a;
+    a = c;
+} while((a+b)<=n);
+
    return 0;
 }
